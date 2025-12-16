9c6c21b820db0211746e0c3c0676b6a46158b944
Maja055 <maja.chojnacka.05.10@gmail.com>
2025-12-16 14:14:41 +0100

    Add files via upload

---
+++ var.py
@@ -0,0 +1,29 @@
+t_skin = 0.003
+t_spar = 0.010
+n_string_upper = 8
+n_string_lower = 8
+A_stringer = 4.0*10**(-4)
+b = 22.15*2
+E = 72.4*10**9 #Elastic modulus
+G = 28*10**9 #Shear modulus
+v = E/(2*G)-1
+Cr= 6.53
+taper = 0.2925
+
+t_skin = 2.5            # [mm]
+t_str = 5               # [mm]
+t_spar = 4              # [mm]
+n_spar = 2              
+n_str = 24
+h_str = 40              # [mm]
+w_str = 40              # [mm]
+h_root1 = 840           # [mm]
+h_root2 = 658.148       # [mm]
+h_root3 = 745.951       # [mm]
+
+# "default"
+# t_skin = 0.003
+# t_spar = 0.010
+# n_string_upper = 8
+# n_string_lower = 8
+# A_stringer = 3.2*10**(-4)
