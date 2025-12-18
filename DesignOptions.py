d89a1adfaab5f3374ca73b0699e7c67bbc046f8f
Maja055 <maja.chojnacka.05.10@gmail.com>
2025-12-18 15:30:51 +0100

    Add files via upload

---
+++ DesignOptions.py
@@ -0,0 +1,25 @@
+#Design option 1
+
+t_skin1 = 0.0025
+t_spar1 = 0.004
+n_string_upper1 = 14
+n_string_lower1 = 10
+A_stringer1= 4.1*10**(-4)
+
+#Design option 2
+
+t_skin2 = 0.0022
+t_spar2 = 0.0035
+n_string_upper2 = 15
+n_string_lower2 = 15
+A_stringer2 = 4*10**(-4)
+
+#Design option 3
+
+t_skin3=0.002
+t_spar3=0.003
+n_string_upper3 = 16
+n_string_lower3 = 14
+A_stringer3 = 4*10**(-4)
+
+spar_location3=0.5
